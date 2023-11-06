481347877c5024d77cbc6f38fc858ee579ac006f
mehrdadzeinali <mehrdad.zeinali@epitech.eu>
2023-11-06 16:00:20 +0100

    complete authController register function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+// app.js
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/index');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Include your routes
 app.use('/', routes);
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,38 +1,64 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
 const pool = require('../config/db');
+const {
+    findUserByEmail,
+    createUser
+  } = require('../models/userModel');
 
 
 class AuthController {
+
+    constructor() {
+        this.register = this.register.bind(this);
+      }
+
     isPasswordValid(password) {
+        let errorMessage = '';
+    
         if (password.length < 8 || password.length > 16) {
-            return false;
-          }
-      
-          const lowerCaseRegex = /[a-z]/;
-          const upperCaseRegex = /[A-Z]/;
-          const digitRegex = /[0-9]/;
-          const specialCharRegex = /[!@#\$%\^&\*]/;
-      
-          if (!lowerCaseRegex.test(password) || 
-              !upperCaseRegex.test(password) || 
-              !digitRegex.test(password) || 
-              !specialCharRegex.test(password)) {
-            return false;
-          }
-      
-          const charCount = {};
-          for (const char of password) {
+            errorMessage = 'Password must be 8-16 characters long.';
+            return { valid: false, message: errorMessage };
+        }
+
+        const lowerCaseRegex = /[a-z]/;
+        if (!lowerCaseRegex.test(password)) {
+            errorMessage = 'Password must include at least one lowercase letter.';
+            return { valid: false, message: errorMessage };
+        }
+    
+        const upperCaseRegex = /[A-Z]/;
+        if (!upperCaseRegex.test(password)) {
+            errorMessage = 'Password must include at least one uppercase letter.';
+            return { valid: false, message: errorMessage };
+        }
+    
+        const digitRegex = /[0-9]/;
+        if (!digitRegex.test(password)) {
+            errorMessage = 'Password must include at least one digit.';
+            return { valid: false, message: errorMessage };
+        }
+    
+        const specialCharRegex = /[!@#\$%\^&\*]/;
+        if (!specialCharRegex.test(password)) {
+            errorMessage = 'Password must include at least one special character (!@#$%^&*).';
+            return { valid: false, message: errorMessage };
+        }
+    
+        const charCount = {};
+        for (const char of password) {
             if (charCount[char]) {
-              charCount[char]++;
-              if (charCount[char] > 2) return false;
+                charCount[char]++;
+                if (charCount[char] > 3) {
+                    errorMessage = 'Password must not have more than three identical characters in a row.';
+                    return { valid: false, message: errorMessage };
+                }
             } else {
-              charCount[char] = 1;
+                charCount[char] = 1;
             }
-          }
-          
-          return true;
+        }
+    
+        return { valid: true, message: 'Password is valid.' };
     }
 
     isEmailValid(email) {
@@ -40,23 +66,25 @@ class AuthController {
         return regex.test(email.toLowerCase());
     }
   
-    async register(req, res, next) {
+    register = async (req, res, next) => {
         const { email, password, confirmation_password } = req.body;
     
         if (!email || !password || !confirmation_password) {
             return res.status(400).json({ message: 'Email, password, and confirmation password are required.' });
         }
-
+    
         if (password !== confirmation_password) {
             return res.status(400).json({ message: 'Passwords do not match.' });
         }
     
         try {
-            if (!this.isPasswordValid(password)) {
-                return res.status(400).json({ message: 'Password does not meet complexity requirements. It must be 8-16 characters long, include upper and lower case letters, numbers, and special characters, and not have more than 2 identical characters in a row.' });
+            const { valid, message } = this.isPasswordValid(password);
+            if (!valid) {
+                return res.status(400).json({ message });
             }
         } catch (error) {
-            return res.status(400).json({ message: 'Error in password validation.' });
+            console.error('Password validation error:', error);
+            return res.status(400).json({ message: 'Error in password validation: ' + error.message });
         }
     
         try {
@@ -68,7 +96,7 @@ class AuthController {
         }
       
         try {
-            const existingUser = await User.findOne({ email });
+            const existingUser = await findUserByEmail(email);
             if (existingUser) {
                 return res.status(409).json({ message: 'User already exists with the provided email.' });
             }
@@ -80,29 +108,13 @@ class AuthController {
                 return res.status(500).json({ message: 'Error in password encryption.' });
             }
     
-            const newUser = new User({
-                email,
-                password: hashedPassword,
-                email_verified: false,
-            });
-    
-            await newUser.save();
-    
+            await createUser(email, hashedPassword);
             res.status(201).json({ message: 'Registration successful.' });
         } catch (error) {
-            if (error.name === 'ValidationError') {
-                let message = 'Validation error: ';
-                for (field in error.errors) {
-                    message += `${field} - ${error.errors[field].message}. `;
-                }
-                return res.status(400).json({ message });
-            } else {
-                console.error('Registration error:', error);
-                return res.status(500).json({ message: 'An unexpected error occurred during registration.' });
-            }
+            console.error('Registration error:', error);
+            return res.status(500).json({ message: 'An unexpected error occurred during registration.' });
         }
     }
-    
   }
   
   module.exports = new AuthController();
