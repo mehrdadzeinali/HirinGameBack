98a44e7e6346e43c5266831c4bded61da19e8e66
mehrdadzeinali <mehrdad.zeinali@epitech.eu>
2023-11-06 17:10:38 +0100

    add helmet to secure my auth system

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-// app.js
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const routes = require('./routes/index');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 app.use('/', routes);
